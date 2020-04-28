3f4ad0f74216d8ba3219f6897d794124395792e2
kiomnd2 <kiomnd2@naver.com>
2020-04-29 01:25:50 +0900

    입력값 valid

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -7,23 +7,25 @@
           <el-form
             :label-position="labelPosition"
             label-width="100px"
-            :model="form"
+            :model="loginForm"
+            :rules="rules"
+            ref="loginForm"
           >
-            <el-form-item>
+            <el-form-item prop="id">
               <el-input
                 placeholder="아이디를 입력해주세요."
-                v-model="form.id"
+                v-model="loginForm.id"
               ></el-input>
             </el-form-item>
-            <el-form-item>
+            <el-form-item prop="password">
               <el-input
                 type="password"
                 placeholder="패스워드를 입력해주세요."
-                v-model="form.password"
+                v-model="loginForm.password"
               ></el-input>
             </el-form-item>
             <el-form-item>
-              <el-button type="primary" @submit="btn_login">로그인</el-button>
+              <el-button type="primary" @click="btn_login('loginForm')">로그인</el-button>
               <el-button type="">회원가입</el-button>
             </el-form-item>
           </el-form>
@@ -39,19 +41,38 @@ import { executeLogin } from '../api/login.api';
 export default {
   name: 'Login',
   data() {
+    const validPassword = (rule, value, callback) => {
+      if(value == '') {
+        callback(new Error('패스워드를 입력해 주세요'))
+      }
+    };
+    const validId = (rule, value, callback) => {
+      if(value == '') {
+        callback(new Error('아이디를 입력해 주세요'))
+      }
+    };
     return {
       labelPosition: 'top',
       img_src: 'logo.jpg',
-      form: {
+      loginForm: {
         id: '',
         password: '',
       },
+      rules : {
+        id : [{ validator : validId , trigger : 'blur'}],
+        password : [{ validator : validPassword , trigger : 'blur'}],
+      }
     };
   },
   methods: {
-    btn_login() {
+    btn_login(formname) {
       //검증
-      executeLogin(this.form);
+      console.log();
+      this.$refs[formname].validate((valid) => {
+        if(valid){
+          executeLogin(this.form);
+        }
+      });
     },
   },
 };
@@ -59,7 +80,7 @@ export default {
 
 <style scoped>
 .top {
-  margin-top: 400px;
+  margin-top: 300px;
 }
 
 .login_top {
