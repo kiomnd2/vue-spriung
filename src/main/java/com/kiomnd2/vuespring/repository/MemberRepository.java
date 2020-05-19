5268f6f4aceb026fad897bc316cae83dacc89c6e
kiomnd2 <kiomnd2@naver.com>
2020-05-20 00:16:30 +0900

    id 로 객체 찾는 메서드 생성

--- src/main/java/com/kiomnd2/vuespring/repository/MemberRepository.java
+++ src/main/java/com/kiomnd2/vuespring/repository/MemberRepository.java
@@ -3,6 +3,8 @@ package com.kiomnd2.vuespring.repository;
 import com.kiomnd2.vuespring.entity.Member;
 import org.springframework.data.repository.CrudRepository;
 
-public interface MemberRepository extends CrudRepository<Member,Long> {
+import java.util.Optional;
 
+public interface MemberRepository extends CrudRepository<Member,Long> {
+    Optional<Member> findById(String id);
 }
